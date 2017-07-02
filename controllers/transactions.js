78bc68c04d2537ac18b788acef1730417a003f90
myyusuf <myyusuf1911@gmail.com>
2017-07-02 15:09:34 +0700

    Complete transaction CRUD

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -43,3 +43,53 @@ exports.create = (req, res) => {
     });
   });
 };
+
+exports.update = (req, res) => {
+
+  const transactionId = req.params.transactionId;
+  const isbns = req.body.isbns.split(',');
+
+  Customer.findOne({ memberId: req.body.memberId })
+  .exec((err, customer) => {
+    Book.find({ isbn: { $in: isbns } })
+    .exec((errFindBooks, books) => {
+      const bookIds = books.map((book) => {
+        return book._id;
+      });
+
+      Transaction.findOne({ _id: transactionId })
+      .exec((errFindTransaction, transaction) => {
+        transaction.member = customer._id;
+        transaction.days = req.body.days;
+        transaction.outDate = new Date(req.body.outDate);
+        transaction.dueDate = new Date(req.body.dueDate);
+        transaction.inDate = new Date(req.body.inDate);
+        transaction.fine = req.body.fine;
+        transaction.books = bookIds;
+
+        transaction.save((errSave) => {
+          if (!errSave) {
+            res.send({ result: 'UPDATE_OK' });
+          } else {
+            console.log(errSave);
+            res.status(500).send('Error');
+          }
+        });
+      });
+    });
+  });
+};
+
+exports.delete = (req, res) => {
+  const transactionId = req.params.transactionId;
+  Transaction.findOne({ _id: transactionId }, (err, transaction) => {
+    transaction.remove((saveErr) => {
+      if (!saveErr) {
+        res.send({ result: 'DELETE_OK' });
+      } else {
+        console.log(saveErr);
+        res.status(500).send('Error');
+      }
+    });
+  });
+};
--- routes/transactions.js
+++ routes/transactions.js
@@ -6,5 +6,7 @@ const transactionController = require('../controllers/transactions');
 
 router.get('/', transactionController.find);
 router.post('/', transactionController.create);
+router.put('/:transactionId', transactionController.update);
+router.delete('/:transactionId', transactionController.delete);
 
 module.exports = router;
